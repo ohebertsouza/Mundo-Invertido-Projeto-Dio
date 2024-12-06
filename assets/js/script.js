6724c1bf1dc919cfccf3b25b6c6d31071dd7c6b3
Hebert souza <hebert_rj21@outlook.com>
2024-12-05 21:22:23 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+function switchTheme(){
+  document.body.classList.toggle("dark-theme");
+  document.body.classList.toggle("light-theme");
+
+  const theme = document.body.classList[0];
+  const music = theme === 'light-theme' ? 'normal-world.mpeg' : 'inverted-world.mpeg';
+
+  const audio = document.getElementById('music');
+  audio.src = `assets/musics/${music}`;
+  audio.play();
+  audio.volume = 0.1;
+}
+
+window.addEventListener('click', invertAudio);
+
+function invertAudio(){
+  const audio = document.getElementById('music');
+  audio.play();
+  audio.volume = 0.1;
+}
